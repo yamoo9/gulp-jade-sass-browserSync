e667c4e1703ca84993ba059938f1c8820e60b8d3
Jee Hoon <yamoo9@naver.com>
2015-09-03 14:38:04 +0900

    gulp-compass 추가

--- gulpfile.js
+++ gulpfile.js
@@ -83,6 +83,7 @@ gulp.task('default', [
 	config.sass_engine === 'ruby' ? 'sass:ruby' : 'sass'
 ], function() {
 	browserSync(config.browserSync)
+	gulp.start('images');
 	gulp.start('watch');
 });
 
@@ -131,13 +132,32 @@ gulp.task('sass:ruby', function() {
 		.pipe( reload({stream: true}) );
 });
 
+// Compass
+gulp.task('sass:compass', function() {
+	gulp.src('src/sass/**/*')
+		.pipe(compass({
+			config_file: 'config.rb',
+			css: 'dist/css',
+			sass: 'src/sass',
+			image: 'dist/images'
+		}))
+		.pipe(gulp.dest('dist/css'));
+});
+
+// 업무: images 디렉토리 dist 디렉토리 안으로 이동
+gulp.task('images', function() {
+	return gulp
+			.src('src/images/**/*')
+			.pipe( gulp.dest('dist/images') );
+});
+
 // 명령어 환경의 코드를 Gulp에서 수행할 수 있도록 조치
 gulp.task('clean', shell.task('rm -rf dist src/output'));
 
 // 필히 Ruby, Sass 설치되어 있어야 사용 가능!
-var from   = process.env.from || 'scss';
-var to     = process.env.to || 'sass';
-var input  = process.env.input || 'src/sass';
+var from   = process.env.from   || 'scss';
+var to     = process.env.to     || 'sass';
+var input  = process.env.input  || 'src/sass';
 var output = process.env.output || 'src/output';
 
 gulp.task(
