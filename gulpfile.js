f3de09605c6f009029ba7110786980cafac03b6d
Jee Hoon <yamoo9@naver.com>
2015-09-03 13:01:53 +0900

    스프라이트 수업 자료

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,8 @@ var gulp        = require('gulp'),
 	// CSS 프리프로세서
 	sass         = require('gulp-sass'),
 	rubySass     = require('gulp-ruby-sass'),
-	globImporter = require('sass-glob-importer'),
+	compass      = require('gulp-compass'),
+	autoprefixer = require('gulp-autoprefixer'),
 
 	/* 유틸리티 ---------------------------------- */
 	gulpif       = require('gulp-if'),
@@ -33,16 +34,17 @@ var config = {
 		'pretty': true
 	},
 	// Sass
-	'sass_engine': process.env.sass || 'node', // 'node' or 'ruby'
+	'sass_engine': process.env.sass || 'ruby', // 'node' or 'ruby'
 	'sass': {
 		// compact, compressed, nested, expanded
 		'outputStyle' : 'expanded',
-		'importer'    : globImporter(),
 	},
 	'ruby_sass': { // 옵션: Git Bash or Terminal ⇒ sass -h
-		'default-encoding' : 'utf-8',    // Windows 환경에서 CP949 오류 발생 시
+		'defaultEncoding'  : 'UTF-8',    // Windows 환경에서 CP949 오류 발생 시
 		'style'            : 'expanded', // compact, compressed, nested, expanded
 		'sourcemap'        : true,
+		'compass'          : true,
+		'require'          : ['bourbon'],
 		// 'no-cache'         : true
 	},
 	'ruby_sass_sourcemaps': {
@@ -50,13 +52,18 @@ var config = {
 		'options': {
 			'includeContent' : false,
 			'sourceRoot'     : 'source',
-			'require'        : ['sass-globbing'],
 		},
 	},
+	// 브라우저 리스트 참고 URL: https://github.com/ai/browserslist#queries
+	'autoprefixer': [
+		'> 1%',
+		'last 2 versions'
+	],
+
 	// Browser-sync
 	'browserSync': { // 옵션: http://www.browsersync.io/docs/options/
 		'server'  : ['dist'],
-		'port'    : 8800,
+		'port'    : 8888,
 		'notify'  : false,
 	}
 };
@@ -105,6 +112,7 @@ gulp.task('sass', function() {
 	return gulp.src('src/sass/**/*.scss')
 		.pipe( sourcemaps.init() )
 		.pipe( sass( config.sass ).on('error', sass.logError) )
+		.pipe( autoprefixer(config.autoprefixer) )
 		.pipe( mq() )
 		.pipe( sourcemaps.write('./maps') )
 		.pipe( gulp.dest('dist/css') )
@@ -116,14 +124,12 @@ gulp.task('sass:ruby', function() {
 	// gulp-ruby-sass 사용 시에는 디렉토리 명만 입력할 것!
 	return rubySass('src/sass/', config.ruby_sass)
 		.on('error', rubySass.logError)
+		.pipe( autoprefixer(config.autoprefixer) )
 		.pipe( sourcemaps.write(config.ruby_sass_sourcemaps.dir, config.ruby_sass_sourcemaps.options) )
-		.pipe( mq() )
 		.pipe( gulp.dest('dist/css') )
 		.pipe( filter('**/*.css') )
 		.pipe( reload({stream: true}) );
 });
-
-
 
 // 명령어 환경의 코드를 Gulp에서 수행할 수 있도록 조치
 gulp.task('clean', shell.task('rm -rf dist src/output'));
